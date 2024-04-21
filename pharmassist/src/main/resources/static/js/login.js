897a58c4673fbe4048aaa570157154f8e90baabe
MedSalahArfa1 <mohamedsalaharfa@gmail.com>
2024-04-21 12:13:15 +0100

    Handle login requests

---
+++ pharmassist/src/main/resources/static/js/login.js
@@ -0,0 +1,25 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+(function ($) {
+    "use strict";
+
+    // Spinner
+    let spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+})(jQuery);
